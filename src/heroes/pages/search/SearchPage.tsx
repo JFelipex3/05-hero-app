2eaab7f30539a0b8842b9b3a77eabcbed6607a17
Jhonnatan Machuca <jmachuca@tuxpan.com>
2025-10-29 15:24:47 -0300

    Agregando Diseño Componentes UI Shadcn Implementar Jumbotron y HeroesLayout

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,5 +1,12 @@
+import { CustomJumbotron } from "@/components/custom/CustomJumbotron"
+
 export const SearchPage = () => {
   return (
-    <div>SearchPage</div>
+    <>
+      <CustomJumbotron 
+        title="Búsqueda de SuperHéroes" 
+        description="Descubre, explora y administra super héroes y villanos" 
+      />
+    </>
   )
 }
