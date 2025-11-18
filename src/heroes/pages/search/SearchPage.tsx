7e453219d767712dcfc6b9d44ea67f6ae2dd5a55
Jhonnatan Machuca <jmachuca@tuxpan.com>
2025-11-18 15:52:03 -0300

    Agregar acción de búsqueda de héroes y actualizar SearchPage para mostrar resultados

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -2,8 +2,22 @@ import { CustomBreadcrumbs } from "@/components/custom/CustomBreadcrumbs"
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron"
 import { HeroStats } from "@/heroes/components/HeroStats"
 import { SearchControl } from "./ui/SearchControl"
+import { HeroGrid } from "@/heroes/components/HeroGrid"
+import { useQuery } from "@tanstack/react-query"
+import { useSearchParams } from "react-router"
+import { searchHeroesAction } from "@/heroes/actions/search-heroes.action"
 
 export const SearchPage = () => {
+
+  const [ searchParams ] = useSearchParams();
+  const name = searchParams.get('name') ?? undefined;
+
+  const { data: heroes = [] } = useQuery({
+    queryKey: ['search', { name }],
+    queryFn: () => searchHeroesAction({ name }),
+    staleTime: 1000 * 60 * 5
+  });
+
   return (
     <>
       <CustomJumbotron
@@ -20,6 +34,9 @@ export const SearchPage = () => {
 
       {/* Controls and Filters*/}
       <SearchControl />
+
+      {/* Mostrar personajes */}
+      <HeroGrid heroes={heroes} />
     </>
   )
 }
