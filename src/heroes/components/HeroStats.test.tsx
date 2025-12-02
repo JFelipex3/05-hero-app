5cd5102d90e2750a85addb34340bacf79dad0620
Jhonnatan Machuca <jmachuca@tuxpan.com>
2025-12-02 17:03:45 -0300

    Agregar pruebas y datos de prueba para el componente HeroStats, incluyendo el contexto de héroes favoritos y la representación de porcentajes.

--- src/heroes/components/HeroStats.test.tsx
+++ src/heroes/components/HeroStats.test.tsx
@@ -4,9 +4,36 @@ import { HeroStats } from "./HeroStats";
 import { render, screen } from "@testing-library/react";
 import { useHeroSummary } from "../hooks/useHeroSummary";
 import type { SummaryInformationResponse } from "../types/summary-information.response";
+import { FavoriteHeroProvider } from "../context/FavoriteHeroContext";
 
 vi.mock('../hooks/useHeroSummary');
 const mockUseHeroSummary = vi.mocked(useHeroSummary);
+
+const mockHero = {
+    "id": "1",
+    "name": "Clark Kent",
+    "slug": "clark-kent",
+    "alias": "Superman",
+    "powers": [
+      "Súper fuerza",
+      "Vuelo",
+      "Visión de calor",
+      "Visión de rayos X",
+      "Invulnerabilidad",
+      "Súper velocidad"
+    ],
+    "description": "El Último Hijo de Krypton, protector de la Tierra y símbolo de esperanza para toda la humanidad.",
+    "strength": 10,
+    "intelligence": 8,
+    "speed": 9,
+    "durability": 10,
+    "team": "Liga de la Justicia",
+    "image": "1.jpeg",
+    "firstAppearance": "1938",
+    "status": "Active",
+    "category": "Hero",
+    "universe": "DC"
+  }
 
 const mockSummaryData: SummaryInformationResponse = {
   "totalHeroes": 25,
@@ -85,7 +112,9 @@ const renderHeroStats = ( mockData?: Partial<SummaryInformationResponse>) => {
 
   return render(
     <QueryClientProvider client={queryClient}>
-      <HeroStats />
+      <FavoriteHeroProvider>
+        <HeroStats />
+      </FavoriteHeroProvider>
     </QueryClientProvider>
   )
 }
@@ -107,5 +136,17 @@ describe('HeroStats', () => {
     expect(screen.getByText('Favoritos')).toBeDefined();
     expect(screen.getByText('Más Fuerte')).toBeDefined();
 
+  });
+
+  it('should change the perrcentage of favorites when a hero is added to favorites', () => {
+    localStorage.setItem('favorites', JSON.stringify([mockHero]));
+
+    renderHeroStats(mockSummaryData);
+
+    const favoritePercentageElement = screen.getByTestId('favorite-percentage');
+    expect(favoritePercentageElement.innerHTML).toContain('4.00%');
+
+    const favoriteCountElement = screen.getByTestId('favorite-count');
+    expect(favoriteCountElement.innerHTML).toContain('1');
   });
 });
